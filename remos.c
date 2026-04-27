0d964538fd388a1079b776ef4ebd9890d578884f
TMJP-E <tellez.morones.joshua.paul@gmail.com>
2026-04-27 06:35:18 -0600

    refactor: Organized drawing functions and started work on file management.

--- remos.c
+++ remos.c
@@ -8,10 +8,10 @@
  *   Salida
  *
  * Las diferentes funciones requeridas son las siguientes:
- * Desplegar Logo
- * Desplegar Menu Principal
- * Desplegar Menu Opciones
- *   Crear archivo de opciones
+ * #Desplegar Logo
+ * #Desplegar Menu Principal
+ * #Desplegar Menu Opciones
+ *   Crear estructura de archivos
  *   Leer archivo de opciones
  *   (Agregar | Modificar | Eliminar) palabras clave
  *   Nombrar bitacora
@@ -22,7 +22,8 @@
  *   Encabezados de bitacora
  *   Lectura, guardado y conteo de lineas en la terminal
  *
- * +Se omite hacer mencion a la entrada del usuario, ya que es mejor practica manejarla en el flujo principal, no en una funcion separada.
+ * # = Completado
+ * Se omite hacer mencion a la entrada del usuario, ya que es mejor practica manejarla en el flujo principal, no en una funcion separada.
  *
  * Realizado por Joshua Tellez, bajo el equipo Remos, en la asignatura Programacion Estructurada.
  */
@@ -30,9 +31,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+
+/**
+ * @brief Desplegar el logo, de manera estatica.
+ */
+void drawLogo()
+{
+    char *logo = "\n"
+                 " ______       ______      ___ __ __      ______      ______      \n"
+                 "/_____/\\     /_____/\\    /__//_//_/\\    /_____/\\    /_____/\\     \n"
+                 "\\:::_ \\ \\    \\::::_\\/_   \\::\\| \\| \\ \\   \\:::_ \\ \\   \\::::_\\/_    \n"
+                 " \\:(_) ) )_   \\:\\/___/\\   \\:.      \\ \\   \\:\\ \\ \\ \\   \\:\\/___/\\   \n"
+                 "  \\: __ `\\ \\   \\::___\\/_   \\:.\\-/\\  \\ \\   \\:\\ \\ \\ \\   \\_::._\\:\\  \n"
+                 "   \\ \\ `\\ \\ \\   \\:\\____/\\   \\. \\  \\  \\ \\   \\:\\_\\ \\ \\    /____\\:\\ \n"
+                 "    \\_\\/ \\_\\/    \\_____\\/    \\__\\/ \\__\\/    \\_____\\/    \\_____\\/ \n"
+                 "\n";
+
+    printf("%s", logo);
+}
+
+/**
+ * @brief Desplegar el menu principal con tres opciones.
+ */
+void drawMenu()
+{
+    char *menu = ""
+                 "\t1. Iniciar\n"
+                 "\t2. Opciones\n"
+                 "\t3. Salida\n";
+
+    printf("%s", menu);
+}
+
+/**
+ * @brief Desplegar el menu de opciones.
+ */
+void drawOptions()
+{
+    char *menu = ""
+                 "\t1. Palabras clave\n"
+                 "\t2. Bitacora\n"
+                 "\t3. Regresar\n";
+
+    printf("%s", menu);
+}
+
+/**
+ * @brief Verifica y crea un directorio.
+ *
+ * @param path Direccion y nombre del directorio.
+ *
+ * @return Confirmacion del estado del directorio. `-1` indica que ya existe el directorio en la direccion y nombre especificados, `0` indica que se creo el directorio, `1` indica que no se creo el directorio.
+ */
+int createDir(const char *path)
+{
+    struct stat filebuf;
+    if (stat(path, &filebuf) == 0)
+    {
+        return -1;
+    }
+
+    if (mkdir(path, 0755) == 0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+void createFile()
+{
+}
+
+/**
+ * @brief Inicializa los archivos necesarios para el programa.
+ *
+ * @details
+ * Crea el directorio `remos/`, adentro, crea el directorio `logs/` y el archivo `config.cfg`.
+ *
+ * @see createDir(), createFile()
+ */
+void initializeFileStructure()
+{
+    const char *root_dir = "remos";
+    const char *log_dir = "logs";
+    const char *config_file = "remos";
+    char aux[32] = {0};
+
+    // Para que el proceso solo dependa de los nombres estaticos, se concatenan las variables.
+    strcat(aux, root_dir);
+    strcat(aux, "/");
+    strcat(aux, log_dir);
+    if (mkdir(aux, 0755) == 0)
+    {
+        printf("Directorio de bitacoras creado.\n");
+    }
+    else
+    {
+        printf("Error al crear el directorio de bitacoras.\n");
+    }
+}
 
 // Funcion para inicializar los archivos de configuracion.
 void initializer(char config_filename[], char wordlist_filename[], char logs_filename[])
@@ -86,33 +186,6 @@ void initializer(char config_filename[], char wordlist_filename[], char logs_fil
         fprintf(wordlist_file, "%s", default_keywords);
     }
     fclose(wordlist_file);
-}
-
-// Funcion para imprimir el logo.
-void draw_logo()
-{
-    char *logo = "\n"
-                 " ______       ______      ___ __ __      ______      ______      \n"
-                 "/_____/\\     /_____/\\    /__//_//_/\\    /_____/\\    /_____/\\     \n"
-                 "\\:::_ \\ \\    \\::::_\\/_   \\::\\| \\| \\ \\   \\:::_ \\ \\   \\::::_\\/_    \n"
-                 " \\:(_) ) )_   \\:\\/___/\\   \\:.      \\ \\   \\:\\ \\ \\ \\   \\:\\/___/\\   \n"
-                 "  \\: __ `\\ \\   \\::___\\/_   \\:.\\-/\\  \\ \\   \\:\\ \\ \\ \\   \\_::._\\:\\  \n"
-                 "   \\ \\ `\\ \\ \\   \\:\\____/\\   \\. \\  \\  \\ \\   \\:\\_\\ \\ \\    /____\\:\\ \n"
-                 "    \\_\\/ \\_\\/    \\_____\\/    \\__\\/ \\__\\/    \\_____\\/    \\_____\\/ \n"
-                 "\n";
-
-    printf("%s", logo);
-}
-
-// Funcion para imprimir el menu principal.
-void draw_menu()
-{
-    char *menu = ""
-                 "\t1. Iniciar\n"
-                 "\t2. Opciones\n"
-                 "\t3. Salida\n";
-
-    printf("%s", menu);
 }
 
 // Funcion de la ejecucion principal.
