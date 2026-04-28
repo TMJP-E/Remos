0729fa1f75101933c78ba71561cf0e70d2ddc529
TMJP-E <tellez.morones.joshua.paul@gmail.com>
2026-04-27 22:28:18 -0600

    refactor: Verify directory and file functions for the main flow.

--- remos.c
+++ remos.c
@@ -11,7 +11,7 @@
  * #Desplegar Logo
  * #Desplegar Menu Principal
  * #Desplegar Menu Opciones
- *   Crear estructura de archivos
+ *   #Crear estructura de archivos
  *   -Leer archivo de opciones
  *   -(Agregar | Modificar | Eliminar) palabras clave
  *   -Nombrar bitacora
@@ -19,7 +19,7 @@
  *   -Desactivar Webhook
  *   -Verificacion Webhook
  * Iniciar ejecucion
- *   Verificar archivo de opciones
+ *   #Verificar archivo de opciones
  *   Desplegar configuracion actual
  *   Creacion de bitacora
  *   Encabezados de bitacora
@@ -41,7 +41,9 @@
 #define MAIN_DIR "remos"
 #define LOGS_DIR "logs"
 #define CONFIG_FILE "config.cfg"
+#define CONFIG_LENGTH 4096
 #define INPUT_LENGTH 1024
+#define KEYS_COUNT 4
 
 /**
  * @brief Despliega el logo, de manera estatica.
@@ -89,17 +91,46 @@ void drawOptions()
 }
 
 /**
+ * @brief Verifica si existe el directorio ingresado.
+ *
+ * @param path Direccion y nombre del directorio
+ *
+ * @return Si existe el directorio o no.
+ */
+bool verifyDirExists(const char *path)
+{
+    struct stat filebuf;
+    return (stat(path, &filebuf) == 0);
+}
+
+/**
+ * @brief Verifica si existe el archivo ingresado.
+ *
+ * @param path Direccion y nombre del directorio
+ *
+ * @return Si existe el archivo o no.
+ */
+bool verifyFileExists(const char *filename)
+{
+    FILE *temporal_pointer = open(filename, "r");
+    bool exists = temporal_pointer != NULL;
+    fclose(temporal_pointer);
+    return exists;
+}
+
+/**
  * @brief Verifica y crea un directorio.
  *
  * @param path Direccion y nombre del directorio.
  *
  * @return Confirmacion del estado del directorio. `-1` indica que ya existe el directorio en la direccion y nombre especificados, `0` indica que se creo el directorio, `1` indica que no se pudo crear el directorio.
+ *
+ * @see verifyDir(), verifyFile()
  */
 int createDir(const char *path, unsigned int mode)
 {
-    struct stat filebuf;
-    // Si el directorio ya existe, carga el directorio en el buffer usando `stat()`
-    if (stat(path, &filebuf) == 0)
+
+    if (verifyDirExists(path))
     {
         return -1;
     }
@@ -126,9 +157,8 @@ int createFile(const char *filename)
     FILE *temporal_pointer;
 
     // Si el archivo ya existe
-    if (temporal_pointer = fopen(filename, "r") != NULL)
+    if (verifyFileExists(filename))
     {
-        fclose(temporal_pointer);
         return -1;
     }
 
@@ -175,61 +205,45 @@ void logDirOrFile(int result, char *name, char *type)
     }
 }
 
-/* DEPRECATED
-// Funcion para inicializar los archivos de configuracion.
-void initializer(char config_filename[], char wordlist_filename[], char logs_filename[])
+/**
+ * @brief Realiza una validacion en el archivo de configuracion para que tenga un formato acorde a la especficacion.
+ *
+ * @details Lee cada linea por separado, y garantiza que se encuentren las claves de los pares clave-valor definidos en la especificacion,
+ *
+ * @param config_file El archivo de configuracion a validar.
+ *
+ * @return Si tiene un formato valido o no.
+ */
+bool validateConfigFile(FILE *config_file)
 {
-    // Detecta o crea el directorio para la configuracion y bitacoras.
-    char *dir_name = "remos";
-    struct stat statbuf;
-    if (stat(dir_name, &statbuf) != 0)
-    {
-        printf("Directorio de configuracion creado.");
-        mkdir(dir_name, 0755);
-    }
-    else
-    {
-        printf("Directorio de configuracion ya existente.");
-    }
+    bool valid = false;
+    int i = 0;
+    char config_string[CONFIG_LENGTH] = {0};
+    char keys[KEYS_COUNT][10] = {"keywords", "log", "url", "enabled"};
+    char *key_delimiter = "=";
+    char *token;
 
-    char config_filename_local[17] = "remos/";
-    strcat(config_filename_local, config_filename);
-    char wordlist_filename_local[16] = "remos/";
-    strcat(wordlist_filename_local, wordlist_filename);
-    char logs_name[64] = {0};
-
-
-    FILE *config_file = fopen(config_filename_local, "r");
-    if (config_file == NULL)
+    while (fgets(config_string, CONFIG_LENGTH, config_file) != NULL)
     {
-        config_file = fopen(config_filename_local, "w");
-        fprintf(config_file, "%s", logs_filename);
-    }
-    else
-    {
-        fgets(logs_name, 64, config_file);
-        int length = strlen(logs_name);
-        for (size_t i = 0; i < length; i++)
+        token = strtok(config_string, key_delimiter);
+        if (strcmp(token, keys[i]) == 0)
         {
-            logs_filename[i] = logs_name[i];
+            valid = true;
+            i++;
         }
     }
-    fclose(config_file);
 
-    // Crea la lista de palabras por defecto
-    const char *default_keywords = "error,warn,crash,note";
-    FILE *wordlist_file = fopen(wordlist_filename_local, "r");
-    if (wordlist_file == NULL)
-    {
-        wordlist_file = fopen(wordlist_filename_local, "w");
-        fprintf(wordlist_file, "%s", default_keywords);
-    }
-    fclose(wordlist_file);
+    return valid;
 }
-*/
 
 int main(int argc, char *argv[])
 {
+    const char *root_dir = MAIN_DIR;
+    const char *logs_dir = LOGS_DIR;
+    const char *config_file = CONFIG_FILE;
+    int file_result = 0;
+    char aux[32] = {0};
+
     int input_int = 0;
     char input_char[INPUT_LENGTH] = "";
     drawLogo();
@@ -244,22 +258,43 @@ int main(int argc, char *argv[])
         switch (input_int)
         {
         case 1: // Iniciar
-            // TODO Flujo principal del programa
+            // Verificar estructura de archivos existente
+            if (!verifyDirExists(root_dir))
+            {
+                printf("No se encontro el directorio principal.\n");
+                printf("Ingresa al menu de Opciones para crearlo.\n");
+                break;
+            };
+
+            strcpy(aux, root_dir);
+            strcat(aux, "/");
+            strcat(aux, logs_dir);
+            if (!verifyDirExists(aux))
+            {
+                printf("No se encontro el directorio de bitacoras.\n");
+                printf("Ingresa al menu de Opciones para crearlo.\n");
+                break;
+            };
+
+            strcpy(aux, root_dir);
+            strcat(aux, "/");
+            strcat(aux, config_file);
+            if (!verifyFileExists(aux))
+            {
+                printf("No se encontro el archivo de configuracion.\n");
+                printf("Ingresa al menu de Opciones para crearlo.\n");
+                break;
+            }
+
+            // TODO Validar archivo de configuracion, obtener parametros de palabras, bitacora, URL y activacion, alerta de sobreescritura, recuadro de confirmacion, ingresar comando a ejecutar, agregar encabezados, leer linea de comandos, almacenar cada linea detectada con hora e indice, enviar cada linea mediante el Webhook.
+
             break;
         case 2: // Opciones
-            // Flujo para la inicializacion de archivos
-            const char *root_dir = MAIN_DIR;
-            const char *logs_dir = LOGS_DIR;
-            const char *config_file = CONFIG_FILE;
-            char aux[32] = {0};
-            int file_result = 0;
-
+            // Inicializacion de archivos
             file_result = createDir(root_dir, 0755);
             logDirOrFile(file_result, root_dir, "d");
             if (file_result == 1)
-            {
                 break;
-            }
 
             // Para que el proceso solo dependa de los nombres estaticos, se concatenan las variables.
             strcpy(aux, root_dir);
@@ -268,9 +303,7 @@ int main(int argc, char *argv[])
             file_result = createDir(aux, 0755);
             logDirOrFile(file_result, aux, "d");
             if (file_result == 1)
-            {
                 break;
-            }
 
             strcpy(aux, root_dir);
             strcat(aux, "/");
@@ -278,9 +311,7 @@ int main(int argc, char *argv[])
             file_result = createFile(aux);
             logDirOrFile(file_result, aux, "f");
             if (file_result == 1)
-            {
                 break;
-            }
 
             do
             {
@@ -291,14 +322,14 @@ int main(int argc, char *argv[])
 
                 switch (input_int)
                 {
-                case 1: // Palabaras clave
-                    // TODO (Agregar | Modificar | Eliminar) Palabras
+                case 1: // Palabaras clave, ninguna entrada puede poseer el delimitador de nueva linea.
+                    // TODO (Agregar | Modificar | Eliminar) Palabras, Validar que las entradas no posean el delimitador de coma.
                     break;
                 case 2: // Bitacora
-                    // TODO (Nombrar | Modificar) Bitacora
+                    // TODO (Nombrar | Modificar) Bitacora, Validar que el nombre no contenga el delimitador de igual.
                     break;
                 case 3: // Webhook
-                    // TODO (Agregar | Modificar | Eliminar) URL, Toggle de Webhook
+                    // TODO (Agregar | Modificar | Eliminar) URL, Toggle de Webhook, Validar que el nombre no contenga el delimitador de igual.
                     break;
                 default:
                     break;
@@ -307,32 +338,5 @@ int main(int argc, char *argv[])
         }
     } while (input_int != 3);
 
-    /* DEPRECATED
-    char config_filename[] = "config.cfg";
-    char wordlist_filename[] = "words.txt";
-    char logs_filename[64] = "logs";
-
-    initializer(config_filename, wordlist_filename, logs_filename);
-
-    int menu_input = 0;
-    while (menu_input != 3)
-    {
-        draw_logo();
-        draw_menu();
-
-        scanf("%d", &menu_input);
-        switch (menu_input)
-        {
-        case 1:
-            start();
-            break;
-        case 2:
-            options();
-            break;
-        default:
-            break;
-        }
-    }
-    */
     return 0;
 }
